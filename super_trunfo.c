a34b5f1ddf2ee75a2279b87c2cf8d1500b685874
moreiiraFernanda <nandamoreira02@gmail.com>
2025-03-13 19:33:58 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,22 +4,22 @@ int main () {
     char cidadeCarta1[50] = "Fortaleza";
     char estadoCarta1[10] = "A";
     char codigoCarta1[10] = "A01";
-    int populacaoCarta1 = 65350;
+    int populacaoCarta1 = 6895350;
     int pontosCarta1 = 19;
     float areaCarta1 = 35.6;
-    float pibCarta1 = 37;
+    float pibCarta1 = 376.54;
     float densidadepopulacional1 = (float) populacaoCarta1 / areaCarta1;
-    float pibpercapita1 = (float) pibCarta1 / populacaoCarta1;
+    float pibpercapita1 = (float) populacaoCarta1/ pibCarta1;
 
     char cidadeCarta2[50] = "Florianopolis";
     char estadoCarta2[10] = "B";
     char codigoCarta2[10] = "B01";
-    int populacaoCarta2 = 59980;
+    int populacaoCarta2 = 5998980;
     int pontosCarta2 = 13;
     float areaCarta2 = 15.7;
-    float pibCarta2 = 57;
+    float pibCarta2 = 570.456;
     float densidadepopulacional2 = (float) populacaoCarta2 / areaCarta2;
-    float pibpercapita2 = (float) pibCarta2 / populacaoCarta2;
+    float pibpercapita2 = (float) populacaoCarta2/ pibCarta2;
     
     printf("Informacoes para a carta 1: \n");
     printf("Estado: %s \n",estadoCarta1);
@@ -30,7 +30,7 @@ int main () {
     printf("Area: .%2f km²\n", areaCarta1);
     printf("PIB: %.2f bilhoes de reais\n", pibCarta1);
     printf("Densidade Populacional: %.3f hab/km²\n", densidadepopulacional1);
-    printf("Pib per capita: %.3f reais\n", pibpercapita1);
+    printf("Pib per capita: %.3f reais\n\n", pibpercapita1);
     
     
 
